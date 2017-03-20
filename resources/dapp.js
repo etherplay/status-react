a5664bced954dc5a24e3f01066722d4e17217fc5
alwx <alwxndr@gmail.com>
2017-03-20 18:15:20 +0300

    Indentation fix

--- resources/dapp.js
+++ resources/dapp.js
@@ -1,32 +1,32 @@
 I18n.translations = {
-  en: {
-    browse_title: 'Browser',
-    browse_description: 'Launch the browser'
-  }
+    en: {
+        browse_title: 'Browser',
+        browse_description: 'Launch the browser'
+    }
 };
 
 status.command({
-  name: "browse",
-  fullscreen: true,
-  title: I18n.t('browse_title'),
-  description: I18n.t('browse_description'),
-  params: [{
-    name: "url",
-    placeholder: "url",
-    type: status.types.TEXT
-  }],
-  onSend: function (params, context) {
-    var url = params.args.url;
-    if (!/^[a-zA-Z-_]+:/.test(url)) {
-      url = 'http://' + url;
-    }
+    name: "browse",
+    fullscreen: true,
+    title: I18n.t('browse_title'),
+    description: I18n.t('browse_description'),
+    params: [{
+        name: "url",
+        placeholder: "url",
+        type: status.types.TEXT
+    }],
+    onSend: function (params, context) {
+        var url = params.args.url;
+        if (!/^[a-zA-Z-_]+:/.test(url)) {
+            url = 'http://' + url;
+        }
 
-    return {
-      title: "Browser",
-      dynamicTitle: true,
-      markup: status.components.bridgedWebView(url)
-    };
-  }
+        return {
+            title: "Browser",
+            dynamicTitle: true,
+            markup: status.components.bridgedWebView(url)
+        };
+    }
 });
 
 status.autorun("browse");
